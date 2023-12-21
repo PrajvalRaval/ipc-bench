671c99f43d1281d39fc409b072a1025a1401d44a
Raval Prajval <ravalprajval@gmail.com>
2023-12-21 17:50:36 +0530

    Update server.c

--- source/shm/server.c
+++ source/shm/server.c
@@ -123,7 +123,7 @@ int main(int argc, char* argv[]) {
 		throw("Error attaching segment");
 	}
 
-	strcpy(tun_name, "tun0");
+	strcpy(tun_name, "tun1");
   	tunfd = tun_alloc(tun_name, IFF_TUN);
 
 	communicate(tunfd, shared_memory, &args);
